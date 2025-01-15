12d1ba85523bd6e93f9f5c7a989dab6e30fcf1a5
Jamie McStay <jamiemcstay@hotmail.com>
2025-01-15 17:41:09 +0000

    Remove accidental block in mobile-header

--- products/views.py
+++ products/views.py
@@ -24,5 +24,5 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
 
